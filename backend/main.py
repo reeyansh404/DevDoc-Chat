0e90005bba3a9fb50ddef0fcd7a331c7dc4f8589
reeyansh404 <121353254+reeyansh404@users.noreply.github.com>
2026-06-24 13:52:35 +1000

    make backend production ready for deployment

--- backend/main.py
+++ backend/main.py
@@ -42,5 +42,11 @@ class Question(BaseModel):
 def ask(question:Question):
     answer = ask_question(question.query)
     return{"answer": answer}  
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
     
 
